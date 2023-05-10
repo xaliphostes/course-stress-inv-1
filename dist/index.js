ccf25fd9f0660442aabaea0e371e57649ed24344
Xaliphostes <fmaerten@gmail.com>
2023-05-10 09:14:35 +0200

    change font

--- dist/index.js
+++ dist/index.js
@@ -53,12 +53,12 @@ function randomStress() {
 }
 exports.randomStress = randomStress;
 function displaySolution(s) {
-    console.log('Iter ' + s.iteration);
-    console.log('S1   ' + s.S1.toFixed(3));
-    console.log('S2   ' + s.S2.toFixed(3));
-    console.log('Theta' + Math.round(s.theta) + '°');
-    console.log('Cost ' + s.cost.toFixed(2));
-    console.log('Fit  ' + Math.round((1 - s.cost) * 100) + '%');
+    console.log('Iter : ' + s.iteration);
+    console.log('S1   : ' + s.S1.toFixed(3));
+    console.log('S2   : ' + s.S2.toFixed(3));
+    console.log('Theta: ' + Math.round(s.theta) + '°');
+    console.log('Cost : ' + s.cost.toFixed(2));
+    console.log('Fit  : ' + Math.round((1 - s.cost) * 100) + '%');
     console.log('');
 }
 exports.displaySolution = displaySolution;
